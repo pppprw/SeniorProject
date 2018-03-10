28fae98c795a9f3ff1e374d75b23aafeb36e10c2
pppprw <p.ppraeww@hotmail.com>
2018-03-10 20:02:48 +0700

    Wander 1.2.3

--- senior/PinViewController.swift
+++ senior/PinViewController.swift
@@ -10,21 +10,22 @@ import UIKit
 import MHPinterestLayout
 import SwiftCommonUtils
 
-typealias Dimension2 = (captionHeight:CGFloat, attachmentHeight:CGFloat)
+typealias Dimension2 = (captionHeight2:CGFloat, attachmentHeight2:CGFloat)
 class PinViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, MHPinterestLayoutDelegate {
     
     var pins = pin.createList()
 
     @IBOutlet weak var PinCollectionView: UICollectionView!
     
-     var layout = MHPinterestLayout()
+     var layout2 = MHPinterestLayout()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        layout.delegate = self
-        layout.headerHeight = 10
-        layout.footerHeight = 10
+        layout2.delegate = self
+        layout2.headerHeight = 10
+        layout2.footerHeight = 10
+        self.PinCollectionView.collectionViewLayout = layout2
     
 
     }
@@ -53,21 +54,21 @@ class PinViewController: UIViewController, UICollectionViewDelegate, UICollectio
     
     func collectionView(_ collectionView: UICollectionView, heightForItemAtIndexPath indexPath: IndexPath) -> CGFloat {
         
-        let caption = pins[indexPath.row].title
-        let postedby = pins[indexPath.row].name
-        let itemWidth:CGFloat = layout.cellContentWidth
+        let caption2 = pins[indexPath.row].title
+        let postedby2 = pins[indexPath.row].name
+        let itemWidth2:CGFloat = layout2.cellContentWidth
         
-        let captionHeight = caption?
-            .computedHeight(atWidth: itemWidth, font: UIFont.systemFont(ofSize: 15))
-        let postedbyHeight = postedby?.computedHeight(atWidth: itemWidth, font: UIFont.systemFont(ofSize: 12))
+        let captionHeight2 = caption2?
+            .computedHeight(atWidth: itemWidth2, font: UIFont.systemFont(ofSize: 15))
+        let postedbyHeight2 = postedby2?.computedHeight(atWidth: itemWidth2, font: UIFont.systemFont(ofSize: 12))
         
-        let imageHeight = pins[indexPath.row].img
-            .computedHeight(atWidth: itemWidth) ?? 0
+        let imageHeight2 = pins[indexPath.row].img
+            .computedHeight(atWidth: itemWidth2) ?? 0
         
-        let yPaddings:CGFloat = 10
+        let yPaddings2:CGFloat = 10
         
         
-        return captionHeight! + postedbyHeight! + imageHeight + yPaddings
+        return captionHeight2! + postedbyHeight2! + imageHeight2 + yPaddings2
     }
     
     
